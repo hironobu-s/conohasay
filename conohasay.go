0721b56f16effa42bc6c0111ea177b2b70c1ae2f
Hironobu Saitoh <hiro@hironobu.org>
2017-12-10 21:25:50 +0900

    関数名をちょっと変えた

--- conohasay.go
+++ conohasay.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
 	"sort"
 	"strings"
+
+	runewidth "github.com/mattn/go-runewidth"
+	"github.com/urfave/cli"
 )
 
 func listCows() []string {
@@ -22,4 +28,87 @@ func listCows() []string {
 
 	sort.Strings(list)
 	return list
+}
+
+func conohasay(input string, maxWrapsize int) string {
+	rows := strings.Split(input, "\n")
+
+	// calculate wrapsize
+	wrapsize := 0
+	for _, line := range rows {
+		s := runewidth.StringWidth(line)
+		if s > maxWrapsize {
+			wrapsize = maxWrapsize
+			break
+		} else if s > wrapsize {
+			wrapsize = s
+		}
+	}
+
+	if wrapsize == maxWrapsize {
+		tmp := make([]string, 0, len(rows)*2)
+		for _, line := range rows {
+			wrapped := strings.Split(runewidth.Wrap(line, maxWrapsize), "\n")
+			tmp = append(tmp, wrapped...)
+		}
+		rows = tmp
+	}
+
+	buf := bytes.NewBuffer(make([]byte, 0, len(input)*2))
+
+	// header
+	buf.WriteString(" " + strings.Repeat("_", wrapsize+2) + " " + newline)
+
+	// message
+	l := len(rows)
+	for i, line := range rows {
+		line = strings.Trim(line, "\r\n\t")
+		line = strings.Replace(line, "\t", "", -1)
+
+		if i == 0 && l == 1 {
+			line = "< " + runewidth.FillRight(line, wrapsize) + ` >`
+		} else if i == 0 {
+			line = "/ " + runewidth.FillRight(line, wrapsize) + ` \`
+		} else if i == l-1 {
+			line = `\ ` + runewidth.FillRight(line, wrapsize) + " /"
+		} else {
+			line = "| " + runewidth.FillRight(line, wrapsize) + " |"
+		}
+		buf.WriteString(line + "\n")
+	}
+
+	// footer
+	buf.WriteString(" " + strings.Repeat("-", wrapsize+2) + " " + newline)
+	buf.WriteString(`    \` + newline)
+	buf.WriteString(`     \` + newline)
+
+	return buf.String()
+}
+
+func loadCow(ctx *cli.Context) (aa string, width int, err error) {
+	name := ctx.String("mikumo")
+	if name != "conoha" && name != "anzu" && name != "umemiya" && name != "logo" {
+		return aa, width, fmt.Errorf("Undefined character name. [%s]", name)
+	}
+
+	size := ctx.String("size")
+	if size != "s" && size != "m" && size != "l" {
+		return aa, width, fmt.Errorf("Undefined image size. [%s]", name)
+	}
+
+	file := name + "-" + size + ".cow"
+	f, err := Assets.Open(file)
+	if err != nil {
+		return aa, width, fmt.Errorf("Could not load the character. [name=%s, size=%s]", name, size)
+	}
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		return aa, width, err
+	}
+
+	aa = string(data)
+	width = strings.Index(aa, "\n")
+
+	return aa, width, nil
 }
--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strings"
 
-	"github.com/mattn/go-runewidth"
 	"github.com/urfave/cli"
 )
 
@@ -54,14 +51,14 @@ func action(ctx *cli.Context) error {
 		return err
 	}
 
-	message := readMessage(ctx)
-	message = formatMessage(message, width/2)
+	message := scanMessage(ctx)
+	message = conohasay(message, width/2)
 
 	fmt.Fprintf(os.Stdout, message+aa)
 	return nil
 }
 
-func readMessage(ctx *cli.Context) string {
+func scanMessage(ctx *cli.Context) string {
 	var input string
 
 	// body
@@ -86,87 +83,4 @@ func readMessage(ctx *cli.Context) string {
 		}
 	}
 	return strings.Trim(input, "\r\n \t")
-}
-
-func formatMessage(input string, maxWrapsize int) string {
-	rows := strings.Split(input, "\n")
-
-	// calculate wrapsize
-	wrapsize := 0
-	for _, line := range rows {
-		s := runewidth.StringWidth(line)
-		if s > maxWrapsize {
-			wrapsize = maxWrapsize
-			break
-		} else if s > wrapsize {
-			wrapsize = s
-		}
-	}
-
-	if wrapsize == maxWrapsize {
-		tmp := make([]string, 0, len(rows)*2)
-		for _, line := range rows {
-			wrapped := strings.Split(runewidth.Wrap(line, maxWrapsize), "\n")
-			tmp = append(tmp, wrapped...)
-		}
-		rows = tmp
-	}
-
-	buf := bytes.NewBuffer(make([]byte, 0, len(input)*2))
-
-	// header
-	buf.WriteString(" " + strings.Repeat("_", wrapsize+2) + " " + newline)
-
-	// message
-	l := len(rows)
-	for i, line := range rows {
-		line = strings.Trim(line, "\r\n\t")
-		line = strings.Replace(line, "\t", "", -1)
-
-		if i == 0 && l == 1 {
-			line = "< " + runewidth.FillRight(line, wrapsize) + ` >`
-		} else if i == 0 {
-			line = "/ " + runewidth.FillRight(line, wrapsize) + ` \`
-		} else if i == l-1 {
-			line = `\ ` + runewidth.FillRight(line, wrapsize) + " /"
-		} else {
-			line = "| " + runewidth.FillRight(line, wrapsize) + " |"
-		}
-		buf.WriteString(line + "\n")
-	}
-
-	// footer
-	buf.WriteString(" " + strings.Repeat("-", wrapsize+2) + " " + newline)
-	buf.WriteString(`    \` + newline)
-	buf.WriteString(`     \` + newline)
-
-	return buf.String()
-}
-
-func loadCow(ctx *cli.Context) (aa string, width int, err error) {
-	name := ctx.String("mikumo")
-	if name != "conoha" && name != "anzu" && name != "umemiya" && name != "logo" {
-		return aa, width, fmt.Errorf("Undefined character name. [%s]", name)
-	}
-
-	size := ctx.String("size")
-	if size != "s" && size != "m" && size != "l" {
-		return aa, width, fmt.Errorf("Undefined image size. [%s]", name)
-	}
-
-	file := name + "-" + size + ".cow"
-	f, err := Assets.Open(file)
-	if err != nil {
-		return aa, width, fmt.Errorf("Could not load the character. [name=%s, size=%s]", name, size)
-	}
-
-	data, err := ioutil.ReadAll(f)
-	if err != nil {
-		return aa, width, err
-	}
-
-	aa = string(data)
-	width = strings.Index(aa, "\n")
-
-	return aa, width, nil
 }
